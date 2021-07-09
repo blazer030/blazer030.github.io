39b1a6de98acb616fb9e4c95e1082e6cad4901f4
Blazer <blazer030@gmail.com>
2021-07-09 19:51:48 +0800

    [add]第七週 - 網頁美感大揭密

---
+++ week7/src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('../views/Front.vue'),
+    children: [
+      {
+        path: 'products',
+        component: () => import('../views/front/Products.vue'),
+      },
+      {
+        path: 'product/:id',
+        component: () => import('../views/front/Product.vue'),
+      },
+      {
+        path: 'cart',
+        component: () => import('../views/front/Cart.vue'),
+      },
+    ],
+  },
+  {
+    path: '/admin',
+    component: () => import('../views/admin/Login.vue'),
+  },
+  {
+    path: '/admin',
+    component: () => import('../views/Admin.vue'),
+    children: [
+      {
+        path: 'products',
+        component: () => import('../views/admin/Products.vue'),
+      },
+      {
+        path: 'orders',
+        component: () => import('../views/admin/Orders.vue'),
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
---
+++ week7/src/views/Admin.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="sb-admin-2">
+    <div id="wrapper">
+      <!-- Sidebar -->
+      <ul
+        class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+        id="accordionSidebar"
+      >
+        <!-- Sidebar - Brand -->
+        <router-link
+          class="sidebar-brand d-flex align-items-center justify-content-center"
+          to="/admin/products"
+        >
+          <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+          </div>
+          <div class="sidebar-brand-text mx-3">My Admin</div>
+        </router-link>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0" />
+
+        <!-- Nav Item - Dashboard -->
+        <li
+          v-for="menu in menuList"
+          :key="menu.id"
+          class="nav-item"
+          :class="menu.active ? 'active' : ''"
+        >
+          <router-link :to="menu.path" class="nav-link">
+            <i :class="menu.icon"></i>
+            <span class="ms-1">{{ menu.name }}</span>
+          </router-link>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block" />
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+          <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
+      </ul>
+      <!-- End of Sidebar -->
+
+      <!-- Content Wrapper -->
+      <div id="content-wrapper" class="d-flex flex-column">
+        <!-- Main Content -->
+        <div id="content">
+          <!-- Topbar -->
+          <nav
+            class="
+              navbar navbar-expand navbar-light
+              bg-white
+              topbar
+              mb-4
+              static-top
+              shadow
+              px-3
+            "
+          >
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle me-3">
+              <i class="fa fa-bars"></i>
+            </button>
+
+            <!-- Topbar Navbar -->
+            <ul class="navbar-nav ms-auto">
+              <!-- Nav Item - User Information -->
+              <li class="nav-item dropdown no-arrow">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="javascript:void(0);"
+                  id="userDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <span class="me-2 d-none d-lg-inline text-gray-600 small">使用者</span>
+                  <img class="img-profile rounded-circle" src="@/assets/img/undraw_profile.svg" />
+                </a>
+                <!-- Dropdown - User Information -->
+                <div
+                  class="
+                    dropdown-menu dropdown-menu-end
+                    shadow
+                    animated--grow-in
+                  "
+                  aria-labelledby="userDropdown"
+                >
+                  <a
+                    class="dropdown-item"
+                    href="javascript:void(0);"
+                    @click.prevent="$refs.logoutModal.openModal"
+                  >
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>
+                    登出
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </nav>
+          <!-- End of Topbar -->
+
+          <!-- Begin Page Content -->
+          <router-view />
+          <!-- /.container-fluid -->
+        </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright &copy; Ethan Website 2021</span>
+            </div>
+          </div>
+        </footer>
+        <!-- End of Footer -->
+      </div>
+      <!-- End of Content Wrapper -->
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="javascript:void(0);">
+      <i class="fas fa-angle-up"></i>
+    </a>
+    <logout-modal @logout="logout" ref="logoutModal"></logout-modal>
+  </div>
+</template>
+
+<script>
+/* global $ */
+import logoutModal from '@/components/admin/logoutModal.vue';
+
+export default {
+  data() {
+    return {
+      menuList: [
+        {
+          id: 1,
+          icon: 'fas fa-gift',
+          name: '商品管理',
+          path: '/admin/products',
+        },
+        {
+          id: 2,
+          icon: 'fas fa-shopping-cart',
+          name: '訂單管理',
+          path: '/admin/orders',
+        },
+      ],
+    };
+  },
+  components: { logoutModal },
+  methods: {
+    logout() {
+      this.$http
+        .post(`${process.env.VUE_APP_APIURL}/logout`)
+        .then((response) => {
+          if (response.data.success) {
+            this.$refs.logoutModal.hideModal();
+            document.cookie = 'hexToken=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+            this.$router.push('/manager');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.menuList.forEach((menu, index) => {
+      if (menu.path === this.$route.fullPath) {
+        this.menuList[index].active = true;
+      }
+    });
+  },
+  watch: {
+    $route(to) {
+      this.menuList.forEach((menu, index) => {
+        if (menu.path === to.fullPath) {
+          this.menuList[index].active = true;
+        } else {
+          this.menuList[index].active = false;
+        }
+      });
+    },
+  },
+  mounted() {
+    $('#sidebarToggle, #sidebarToggleTop').on('click', () => {
+      $('body').toggleClass('sidebar-toggled');
+      $('.sidebar').toggleClass('toggled');
+      if ($('.sidebar .collapse').length && $('.sidebar').hasClass('toggled')) {
+        $('.sidebar .collapse').collapse('hide');
+      }
+    });
+
+    // Close any open menu accordions when window is resized below 768px
+    $(window).resize(() => {
+      if ($('.sidebar .collapse').length && $(window).width() < 768) {
+        $('.sidebar .collapse').collapse('hide');
+      }
+    });
+
+    // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+    $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function (e) {
+      if ($(window).width() > 768) {
+        const e0 = e.originalEvent;
+        const delta = e0.wheelDelta || -e0.detail;
+        this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+        e.preventDefault();
+      }
+    });
+
+    // Scroll to top button appear
+    $(document).on('scroll', function () {
+      const scrollDistance = $(this).scrollTop();
+      if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
+
+    // Smooth scrolling using jQuery easing
+    $(document).on('click', 'a.scroll-to-top', (e) => {
+      // const $anchor = $(this);
+      $('html, body')
+        .stop()
+        .animate(
+          {
+            scrollTop: $('#page-top').offset().top,
+          },
+          1000,
+          'easeInOutExpo',
+        );
+      e.preventDefault();
+    });
+  },
+};
+</script>
